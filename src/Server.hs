a47daa9f7a21cd20f5d798fdd7c35a3d5f8e03fe
Joseph Kachmar <joseph.kachmar@wework.com>
2016-06-24 11:47:30 -0400

    Propagates error codes from Client to Server a little better

--- src/Server.hs
+++ src/Server.hs
@@ -8,12 +8,14 @@ module Server
 import           Control.Monad.Except       (liftIO)
 import           Control.Monad.IO.Class     (MonadIO)
 import           Control.Monad.Trans.Except (ExceptT (..), runExceptT, throwE)
+import           Network.HTTP.Types         (Status (..))
 import           Network.Wai
 import           Network.Wai.Handler.Warp
 import           Servant                    ((:>), Get, JSON, Proxy (..),
-                                             ServantErr (..), Server, serve)
+                                             ServantErr (..), Server, err404,
+                                             serve)
 import           Servant.Client             (BaseUrl)
-import           Servant.Common.Req         (ServantError)
+import           Servant.Common.Req         (ServantError (..))
 
 import           Client                     (getRespList, urlify)
 import           Types
@@ -67,21 +69,31 @@ server = getDifferences
 
 -- There has to be a better way to do this...
 getDifferences :: ExceptT ServantErr IO Difference
-getDifferences = let resps = convertError (getRespList urls)
-            in go . Right <$> resps
+getDifferences = let resps = liftError (getRespList urls)
+                 in go . Right <$> resps
   where go (Right respList) = getDifference respList
 
 --------------------------------------------------------------------------------
 -- Convert client errors to server errors
 
-convertError :: (Show b, MonadIO m) => ExceptT ServantError m b -> ExceptT ServantErr m b
-convertError apiReq = either
+liftError :: (Show b, MonadIO m) => ExceptT ServantError m b -> ExceptT ServantErr m b
+liftError apiReq = either
   logAndFail
   return =<< ExceptT
   (Right <$> runExceptT apiReq)
 
-logAndFail :: (Show a, Show b, MonadIO m) => a -> ExceptT ServantErr m b
+logAndFail :: (Show b, MonadIO m) => ServantError -> ExceptT ServantErr m b
 logAndFail e = do
   liftIO (putStrLn ("Got internal api error: " ++ show e))
-  throwE internalError
-  where internalError = ServantErr 500 "Internal Server Error" "" []
+  throwE (convertError e)
+
+
+convertError :: ServantError -> ServantErr
+convertError  (FailureResponse (Status code body) _ _) =
+  ServantErr code (show body) "" []
+
+convertError (UnsupportedContentType _ _) =
+  err404 { errBody = "Resource not found"}
+
+convertError _ =
+  ServantErr 500 "Internal Server Error" "" []
